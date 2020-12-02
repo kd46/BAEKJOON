9bbc6da9c1f33897ccf17f592efcb6d711785a32
sfdg9 <sfdg9055@naver.com>
2020-12-02 16:29:43 +0900

    Baekjoon no.2439

---
+++ BAEKJOON/src/d201202/P2439.java
@@ -0,0 +1,30 @@
+package d201202;
+
+import java.io.*;
+
+public class P2439 {
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			int n = Integer.parseInt(br.readLine());
+			for(int i = 1; i <= n; i++) {
+				for(int j = n; j > i; j--) {
+					bw.write(" ");
+				}
+				for(int j = 0; j < i; j++) {
+					bw.write("*");
+				}
+				bw.write("\n");
+			}
+			
+			br.close();
+			bw.flush();
+			bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+}
